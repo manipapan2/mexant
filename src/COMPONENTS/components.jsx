6222383bda3a9be89e4229d459015e4ce5642084
manipapan2 <manipapan2@gmail.com>
2023-09-16 20:19:52 +0330

    Updated

--- src/COMPONENTS/components.jsx
+++ src/COMPONENTS/components.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import '../app.css';
+import '../App.css';
 
 export default function Navigation() {
     useEffect(() => {
